e2742fcd5b1a747d641eddc04dd93addc9bacca6
Diadon81 <a.slastenov@gmail.com>
2017-10-01 03:13:10 +0300

    Fix asteriskInterface to run asterisk command correctly.

--- src/diadon/sms/interfaces/asteriskInterface.java
+++ src/diadon/sms/interfaces/asteriskInterface.java
@@ -6,7 +6,13 @@
 package diadon.sms.interfaces;
 
 import diadon.sms.logic.SmsRaw;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,8 +21,30 @@ import java.io.IOException;
 public class asteriskInterface {
 public static void send(SmsRaw sr, String asterisk, String dongle) {
 String pdu=String.format("%s%s%s%s%s%s%s%s%s%s",sr.getSca(),sr.getPdu_type(),sr.getTp_mr(),sr.getTp_da(),sr.getTp_pid(),sr.getTp_dcs(),sr.getTp_vp(),sr.getTp_udl(),sr.getTp_udh(),sr.getTp_ud()); 
-try {   
-  Process p = new ProcessBuilder(asterisk, "-rx"," 'dongle pdu "+dongle+" "+pdu+"'").start();  
+try { 
+  String[] command={asterisk,"-rx","dongle pdu "+dongle+" "+pdu};
+ // System.out.println(command);
+  //Process p = Runtime.getRuntime().exec(command);
+       Runtime runtime = Runtime.getRuntime();
+
+       Process process = runtime.exec(command);
+
+       InputStream is = process.getInputStream();
+
+       InputStreamReader isr = new InputStreamReader(is);
+
+       BufferedReader br = new BufferedReader(isr);
+
+       String line;
+  
+       while ((line = br.readLine()) != null) {
+
+         System.out.println(line);
+
+       }
+  
+  
+  
 }  
  catch (IOException e) {
    System.out.println("Error to exec Asterisk");  
